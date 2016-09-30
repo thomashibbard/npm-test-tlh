805ac10b708007a52e81374870cb73797112dadf
Thomas Hibbard <thomashibbard@Thomass-MacBook-Pro.local>
2016-09-30 14:49:25 -0500

    adding gitignore

---
+++ index.js
@@ -0,0 +1,6 @@
+var path = require('path');
+var chalk = require('chalk');
+console.log('test');
+console.log(chalk.red(path.join(__dirname, 'testDirectory')));
+console.log(chalk.green('__dirname: ', __dirname));
+console.log(chalk.blue('__filename: ', __filename));
