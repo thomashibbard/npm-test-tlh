7fc33ffacce3aa1beaa3d0d5079d5a76d9a3f2b2
Thomas Hibbard <thomashibbard@Thomass-MacBook-Pro.local>
2016-09-30 15:25:00 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var path = require('path');
-var chalk = require('chalk');
-console.log('test');
-console.log(chalk.red(path.join(__dirname, 'testDirectory')));
-console.log(chalk.green('__dirname: ', __dirname));
-console.log(chalk.blue('__filename: ', __filename));
+#! /usr/bin/env node
+
+exports.print = function(){
+	var path = require('path');
+	var chalk = require('chalk');
+	console.log('test');
+	console.log(chalk.red(path.join(__dirname, 'testDirectory')));
+	console.log(chalk.green('__dirname: ', __dirname));
+	console.log(chalk.blue('__filename: ', __filename));
+}
+
+exports.print();
